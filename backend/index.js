88db4306b150ebf8d58f3fd726c37480a9c94fc7
Santhoji07 <santhoji07msd@gmail.com>
2026-01-02 18:35:29 +0530

    Express Server

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ const pool = require("./db");
 const parseConsent = require("./parser/consentParser");
 const generatePolicy = require("./policy/policyGenerator");
 const consentEnforcer = require("./middleware/consentEnforcer");
+const consentRoutes = require("./routes/consentRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/consent", consentRoutes);
